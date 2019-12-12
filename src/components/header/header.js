1648e4759affa6e9d65416252b20fb400bde12c1
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-12-12 12:20:54 +0700

    Сделал компонент "Header"

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.sass';
+
+const Header = () => (
+  <header className="header">
+    <img src="/img/logo.png" alt="Logo"/>
+  </header>
+)
+
+export default Header;
--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './home.sass';
+import Header from '../../components/header/header'
 import FormAut from '../../components/form-aut/form-aut'
 
 const Home = () => (
-  <div className="container">
-    <h1 className="title">Hello React!</h1>
-    <FormAut />
-  </div>
+  <React.Fragment>
+    <Header />
+    <div className="container">
+      <p className="form-title">Авторизуйтесь на сайте, 
+      чтобы начать им пользоваться<br/> (введите любые логин и пароль).</p>
+      <FormAut />
+    </div>
+  </React.Fragment>
 )
 
 export default Home;
